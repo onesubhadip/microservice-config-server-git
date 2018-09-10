3f8502853bb357047d421bd2513557c58419359e
Subhadip Dey <onesubhadip@gmail.com>
2018-09-10 22:01:33 +0530

    initial commit

---
+++ src/test/java/org/sdey/microserviceconfigservergit/MicroserviceConfigServerGitApplicationTests.java
@@ -0,0 +1,16 @@
+package org.sdey.microserviceconfigservergit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MicroserviceConfigServerGitApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
